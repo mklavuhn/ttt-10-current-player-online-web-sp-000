9fd117aa0737f2d9bda4b456033362b6844a2acd
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-02 20:07:05 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,6 +4,9 @@ def turn_count(board)
     if space == "X" || space == "O"
       counter += 1
     end
+  end
+  counter
+end
 def current_player(board)
   turn_count(board) % 2 == 0? "X" : "O"
 end
